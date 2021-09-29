b48b4485d51f487c9d9778e4263fdcff22215e44
dpermana-ops <denny.permana@gmail.com>
2021-09-29 23:34:52 +0700

    Update main.py with Multiprocessing code

--- main.py
+++ main.py
@@ -34,3 +34,26 @@ finish = time.perf_counter()
 
 executed_time = round(finish - start, 6)
 print(f"Finished in {executed_time} second(s)")
+
+# Multiprocessing
+
+import multiprocessing
+import time
+from find_fibonacci import find_fibonacci
+
+start = time.perf_counter()
+
+processes = []
+for num in range(10):
+  p = multiprocessing.Process(target=find_fibonacci, args=[num])
+  p.start()
+
+  processes.append(p)
+
+for process in processes:
+  process.join()
+
+finish = time.perf_counter()
+
+executed_time = round(finish - start, 6)
+print(f"Finished in {executed_time} second(s)")
