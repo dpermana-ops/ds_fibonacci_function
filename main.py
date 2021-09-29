b444a90c616ef71e052b7dd88ebf0ad77da7d513
dpermana-ops <denny.permana@gmail.com>
2021-09-29 23:32:46 +0700

    Update main.py with Multithreading code

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# Syncrhonous
+
 from find_fibonacci import find_fibonacci
 import time
 
@@ -5,6 +7,28 @@ start = time.perf_counter()
 
 for i in range(100):
   find_fibonacci(i)
+
+finish = time.perf_counter()
+
+executed_time = round(finish - start, 6)
+print(f"Finished in {executed_time} second(s)")
+
+# Multithreading
+
+from find_fibonacci import find_fibonacci
+import time
+import threading
+
+start = time.perf_counter()
+#start = 1.5
+threads = []
+for num in range(100):
+  t = threading.Thread(target=find_fibonacci, args=[num])
+  t.start()
+  threads.append(t)
+
+for thread in threads:
+  thread.join()
 
 finish = time.perf_counter()
 
